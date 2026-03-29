dc1a57bce815d2bb52c520b165e81389eff83b68
Ishrat2413 <ishratrintu2413@gmail.com>
2026-03-29 14:41:59 +0600

    feat(PortalPage): add new components for user roles management, IBG certificates, and mobile design; update DocumentTypes and SecurityCertifications for functionality and user experience

--- app/(home)/portal/page.tsx
+++ app/(home)/portal/page.tsx
@@ -4,14 +4,17 @@ import DocumentManagement from "@/components/portal/document-management";
 import DocumentTypes from "@/components/portal/document-types";
 import GetCompliant from "@/components/portal/get-compliant";
 import HowPlatformWorks from "@/components/portal/how-platform-works";
+import IBGCertificates from "@/components/portal/ibg-certificates";
 import ICOBreach from "@/components/portal/ico-breach";
 import ICORegulations from "@/components/portal/ico-regulations";
 import InstallationCompanies from "@/components/portal/installation-companies";
 import InstallerDashboard from "@/components/portal/installer-dashboard";
+import MobileDesign from "@/components/portal/mobile-design";
 import PortalBanner from "@/components/portal/portal-banner";
 import RenewablyCompliant from "@/components/portal/renewably-compliant";
 import SecurityCertifications from "@/components/portal/security-certifications";
 import SecurityProtection from "@/components/portal/security-protection";
+import UserRolesAndManagement from "@/components/portal/user-roles-management";
 
 export default function PortalPage() {
   return (
@@ -30,6 +33,9 @@ export default function PortalPage() {
       <DocumentTypes />
       <SecurityProtection />
       <SecurityCertifications />
+      <UserRolesAndManagement />
+      <IBGCertificates />
+      <MobileDesign />
     </>
   );
 }
