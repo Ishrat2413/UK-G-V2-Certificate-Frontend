fcb5b09b94e81b61bc441e842d92e1e18a3ae981
Ishrat2413 <ishratrintu2413@gmail.com>
2026-03-31 10:44:14 +0600

    feat(InsurancePage): add BenefitsForHomeowners, BenefitsForInstallers, and ComprehensiveInsurance components; enhance HowIBGWorks component with step-by-step guide

--- app/(home)/insurance/page.tsx
+++ app/(home)/insurance/page.tsx
@@ -1,5 +1,8 @@
 import ApprovedMeasures from "@/components/insurance/approved-measures";
+import BenefitsForHomeowners from "@/components/insurance/benefits-for-homeowners";
+import BenefitsForInstallers from "@/components/insurance/benefits-for-installers";
 import BluedropService from "@/components/insurance/bluedrop-service";
+import ComprehensiveInsurance from "@/components/insurance/comprehensive-insurance";
 import DataGovernance from "@/components/insurance/data-governance";
 import HowIBGWorks from "@/components/insurance/how-ibg-works";
 import InsuranceBanner from "@/components/insurance/insurance-banner";
@@ -15,7 +18,11 @@ export default function Insurance() {
       <WhatIsIBG />
       <HowIBGWorks />
       <DataGovernance />
+      <HowIBGWorks />
       <WhyMultilayer />
+      <BenefitsForInstallers />
+      <BenefitsForHomeowners />
+      <ComprehensiveInsurance />
     </>
   );
 }
